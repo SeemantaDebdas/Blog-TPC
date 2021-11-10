fcd3ea53a327bb135c4c510011546db5fadf36b9
SeemantaDebdas <seemantadebdas@gmail.com>
2021-11-11 01:34:59 +0530

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed;
+    [SerializeField] float rotationSmoothTime;
+
+    [Header("Gravity")]
+    [SerializeField] float gravity = 9.8f;
+    [SerializeField] float gravityMultiplier = 2;
+    [SerializeField] float groundedGravity = -0.5f;
+    [SerializeField] float jumpHeight = 3f;
+    float velocityY;
+
+    CharacterController controller;
+    Camera cam;
+
+    float currentAngle;
+    float currentAngleVelocity;
+
+    private void Awake()
+    {
+        //getting reference for components on the Player
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleGravityAndJump();
+    }
+
+    private void HandleMovement()
+    {
+        //capturing Input from Player
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if (movement.magnitude >= 0.1f)
+        {
+            //compute rotation
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+
+            //move in direction of rotation
+            Vector3 rotatedMovement = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(rotatedMovement * speed * Time.deltaTime);
+        }
+    }
+
+    void HandleGravityAndJump()
+    {
+        //apply groundedGravity when the Player is Grounded
+        if (controller.isGrounded && velocityY < 0f)
+            velocityY = groundedGravity;
+
+        //When Grounded and Jump Button is Pressed, set veloctiyY with the formula below
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocityY = Mathf.Sqrt(jumpHeight * 2f * gravity);
+        }
+
+        //applying gravity when Player is not grounded
+        velocityY -= gravity * gravityMultiplier * Time.deltaTime;
+        controller.Move(Vector3.up * velocityY * Time.deltaTime);
+    }
+}
